7a0a50b663bb7216e99275bcf5536543ef334a32
expband <maxteroidyandex@gmail.com>
2024-06-09 14:39:51 +0300

    Auth

---
+++ static/test.js
@@ -0,0 +1,20 @@
+var axios = require("axios").default;
+
+var options = {
+    method: 'POST',
+    url: 'https://dev-u5le8cq1mkql8c2t.us.auth0.com/oauth/token',
+    headers: {'content-type': 'application/x-www-form-urlencoded'},
+    data: new URLSearchParams({
+        grant_type: 'authorization_code',
+        client_id: 'ItNrfB0zq2owCl8RUoUAc2PvTmpmLAnR',
+        client_secret: 'h-uWEwtf9LAe527iz5nLowW-a4mdXbG2SINLLk6cofxyv95UBw8Jyb_t9hoEr6De',
+        code: 'JiT5RN1kn_oyDAvdGDsnOb1-ezqvfCBSWYqQURJYaaYpD',
+        redirect_uri: 'https://jimmi-newtron-front-1129fbebcd87.herokuapp.com',
+    })
+};
+
+axios.request(options).then(function (response) {
+    console.log(response.data);
+}).catch(function (error) {
+    console.error(error);
+});
