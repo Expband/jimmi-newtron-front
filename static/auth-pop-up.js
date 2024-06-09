b662799faeaabeeb97ca8bb6ae0218af34450fd7
expband <maxteroidyandex@gmail.com>
2024-06-09 14:22:44 +0300

    Auth

--- static/auth-pop-up.js
+++ static/auth-pop-up.js
@@ -12,7 +12,7 @@ $('#authorize').on('click', (e) => {
         }
     }
     var lockPasswordless = new Auth0LockPasswordless(
-        'xKh4doMSl8ByXlLW5pjqZsLLr8fQMmJw', //client_id
+        'ItNrfB0zq2owCl8RUoUAc2PvTmpmLAnR', //client_id
         'dev-u5le8cq1mkql8c2t.us.auth0.com',
         passwordlessOptions
     );
